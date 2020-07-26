a819f5db60331e3ced2e73a14f77318fd07591f8
bowenx2005 <bowenx2005@yahoo.ca>
2020-07-26 15:30:21 -0700

    Added video and thumbnail retrieval, looking into video download

--- src/background/background.js
+++ src/background/background.js
@@ -1,3 +1,34 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
     console.log(request.value)
+    let id = findId(request.value);
+    fireLinksRequests(`https://youtubeaudio.majorcadevs.com/api/${id}/160`);
+    let thumbnailURL = `https://i.ytimg.com/vi/${id}/mqdefault.jpg`
+    console.log(thumbnailURL);
+    chrome.runtime.sendMessage({type:'image', value: thumbnailURL});
 })
+
+let oReq;
+
+function findId(url){
+    let index = url.indexOf('v=');
+    let id = url.slice(index+2);
+    return id;
+}
+
+function fireLinksRequests(endpoint){
+    oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", endpoint); 
+    oReq.send();
+}
+
+function reqListener(){
+    if (oReq.status !== 200) {
+        console.log('Failed request');
+    } else {
+        let response = JSON.parse(oReq.response);
+        let videoURL = response.urls["160"];
+        console.log(videoURL);
+        chrome.runtime.sendMessage({type:'video', value:videoURL});
+    }
+}
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded',()=>{
     document.querySelector('button').addEventListener('click',retrieveUrl,false);
+    chrome.runtime.onMessage.addListener((response,sender, sendResponse)=>{
+        if (response.type === 'image'){
+            document.querySelector('.image').textContent = `Thumbnail Link`;
+            document.querySelector('.image').href = response.value;
+        } else if (response.type === 'video'){
+            document.querySelector('.video').textContent = `Video link`;
+            document.querySelector('.video').href = response.value;
+        } else {
+
+        }
+        return true;
+    })
 });
 
 function retrieveUrl(){
@@ -10,5 +22,5 @@ function retrieveUrl(){
 };
 
 function sendMessage(url){
-    chrome.runtime.sendMessage({value:url})
+    chrome.runtime.sendMessage({value:url});
 }
