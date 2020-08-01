c84fe5b3d88fe9471745ba5da130d611aa8ed0ed
wji98 <vincent.ji1998@gmail.com>
2020-07-31 21:33:37 -0700

    Update find_best_frame.py
    Updated code to give timestamp. Fixed some bugs

--- find_best_frame.py
+++ find_best_frame.py
@@ -8,45 +8,29 @@ Created on Sun Jul 26 22:39:41 2020
 import cv2 as cv
 import time
 
+start = time.time() #start timing
 
-gray = cv.imread('C:\\Users\\xy\\Documents\\thumb1.jpg', cv.IMREAD_GRAYSCALE)
-video_path = 'C:\\Users\\xy\\Downloads\\videoplayback-1.mp4'
-height, width = gray.shape
+#load files/video
+gray = cv.imread('C:\\Users\\xy\\Documents\\thumb2.jpg', cv.IMREAD_GRAYSCALE)
+video_path = 'C:\\Users\\xy\\Downloads\\videoplayback-5.mp4'
 
-orb = cv.ORB_create()
-kp, des = orb.detectAndCompute(gray,None)
+bf = cv.BFMatcher() #feature matching object
 
-bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+sift = cv.xfeatures2d.SIFT_create() #sift detection object
+kp, des = sift.detectAndCompute(gray,None) #get keypoints of thumbnail
 
-cap = cv.VideoCapture(video_path)
-sample_rate = 24
-
-start1 = time.time()
-sift = cv.xfeatures2d.SIFT_create()
-kp, des = sift.detectAndCompute(gray,None)
-end1 = time.time()
-
-#
-print(len(kp))
-print(end1-start1)
-
-cap = cv.VideoCapture(video_path)
-#sample_rate = 30
-sample_rate = 24
+cap = cv.VideoCapture(video_path) #get video
+sample_rate_exact = cap.get(cv.CAP_PROP_FPS) #find frame rate 
+sample_rate = round(sample_rate_exact) #rounded frame rate (for while loop)
 success = cap.grab() # get the next frame
-fno = 0
-best_match = 0
-start = time.time()
+fno = 0 #frame number
+best_match = 0 #maximum number of matches
+best_fno = 0 #frame number of closes match
+
 while success:
-    if fno % (sample_rate) == 0:
+    if fno % (sample_rate) == 0: #sample rate of about 1 frame per second
         _, img = cap.retrieve()
-		#do_something(img)
-        #print(fno)
-        #cv2.imshow(str(fno),img)
-        #cv2.waitKey(0)
         cmp = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-        #cmp = cv.resize(cmp, (960,540))
-        #cmp = cv.resize(cmp, (width,height))
         kp1, des1 = sift.detectAndCompute(cmp,None)
         
         try:
@@ -55,18 +39,33 @@ while success:
             matches = []
         
         good = 0
-        for m,n in matches:
+        for m,n in matches: #ratio test
             if m.distance < 0.75*n.distance:
                 good += 1
                 
         if good > best_match:
             best_match = good
             best_img = img
-            	# read next frame
-        #print(fno, "of 9000")
+            best_fno = fno
+
     fno += 1
     success = cap.grab()
 
 end = time.time()
-print(end-start)
-cv.imwrite('best_match_orb.jpg',best_img)
+cap.release()
+print('Run Time:',end-start)
+
+if (best_match/len(kp) < 0.1): #no match condition
+    print("NO MATCH")
+
+minutes = int((best_fno/sample_rate_exact)//60)
+seconds = round((best_fno/sample_rate_exact)%60)
+
+print(str(minutes)+':'+str(seconds)) #time stamp
+
+cv.imshow('best match',best_img)
+cv.imshow('original image', gray)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
