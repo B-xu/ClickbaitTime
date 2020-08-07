e68ef368c9c875e019b0090ed32172dcaa434f09
bowenx2005 <bowenx2005@yahoo.ca>
2020-08-06 21:24:11 -0700

    Tried removing consts

--- src/server/def_find_frame.py
+++ src/server/def_find_frame.py
@@ -9,8 +9,8 @@ import cv2 as cv
 import numpy as np
 import urllib.request as urllib
 
-url = 'https://i.ytimg.com/vi/MnNLTKa-VFw/mqdefault.jpg'
-video_path = 'https://r4---sn-nx5e6ne6.googlevideo.com/videoplayback?expire=1596499619&ei=Q1IoX461BcmagQen06yoAw&ip=136.243.55.47&id=o-AFWpbJVIhb9mSOuR7jxLTzMx6CSrkgFhi2Kw9VqZcD5g&itag=160&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278%2C394%2C395%2C396%2C397%2C398%2C399&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&gir=yes&clen=7813448&dur=756.920&lmt=1575374040081048&fvip=4&keepalive=yes&fexp=23812954,23883098&c=WEB&txp=5535432&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRQIgSr1X4PgGZRELlk3dlDKgJKJYEZnE01oWKFnwdsGsluECIQDVmG7EDw2Y8hvtFJXtZAfLA1ALaZWGB_GmLHrNZHt5NA%3D%3D&ratebypass=yes&rm=sn-4g5e6r7z&req_id=ae8e7166b5c7a3ee&redirect_counter=2&cm2rm=sn-ni5f-t8gs7z&cms_redirect=yes&ipbypass=yes&mh=pI&mip=24.86.140.150&mm=30&mn=sn-nx5e6ne6&ms=nxu&mt=1596477918&mv=m&mvi=4&pl=22&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgfE1faN-pSp0-yrF3DEWSXse8PD72vHfAUVkGq06ZIaoCIDsypPtdcLGZx0uK8rcJYTSWHaByJchs03mk08Eut-aA'
+# url = 'https://i.ytimg.com/vi/oZmvn_fYWO4/mqdefault.jpg'
+# video_path = 'https://r4---sn-p5qs7ned.googlevideo.com/videoplayback?expire=1596792615&ei=x8osX8PQNYi81gKKgLiYDw&ip=136.243.55.47&id=o-AOee1lAsQPA__TNyind5NXj3cJZNfh-AJP4okGcFE1bY&itag=160&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278%2C394%2C395%2C396%2C397%2C398%2C399&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&gir=yes&clen=4993837&dur=736.720&lmt=1580446414838447&fvip=4&keepalive=yes&fexp=23812954,23883098&c=WEB&txp=5535432&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRQIgV5jri8acjp8npkK2xf2OV0RIsheb2MZsLLWSZSotQqICIQDnEyQX4JCVnYvUT0rdP-0egPgCwblLbaNO5ETEL6E3fA%3D%3D&ratebypass=yes&redirect_counter=1&rm=sn-4g5e6d7z&req_id=fe70bb98ba94a3ee&cms_redirect=yes&ipbypass=yes&mh=gR&mip=24.86.140.150&mm=31&mn=sn-p5qs7ned&ms=au&mt=1596770691&mv=D&mvi=4&pl=0&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgDANuzkpdIXnu6jSK0nzpm2aqs2KkfHU2jNVFC1gyVGICIQCKI47UwE53jWftTPDVswrKb8Yipznnj1n-Mer30JqbMA%3D%3D'
 
 def find_frame(url, video_path):
     
@@ -70,4 +70,4 @@ def find_frame(url, video_path):
     
     return("%d:%02d" % (minutes,seconds)) #time stamp
     
-
+print(find_frame(url,video_path))
