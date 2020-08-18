a101808756601fe9de9cf30e5c2ff6ba52bffb4e
bowenx2005 <bowenx2005@yahoo.ca>
2020-08-17 19:03:18 -0700

    Added youtube dl file

---
+++ src/server/getVideoURL.py
@@ -0,0 +1,33 @@
+import youtube_dl
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'worst',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp4',
+        'preferredquality': '160',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    info_dict = ydl.extract_info(['https://www.youtube.com/watch?v=BaW_jenozKc'], download=False)
+    video_url = info_dict.get("url", None)
+    print(video_url)
